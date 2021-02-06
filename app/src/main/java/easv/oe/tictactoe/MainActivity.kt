b2df0a113bb7666dd22830313e0870fecb4fe9fa
oleeriksen <oe@easv.dk>
2021-02-06 22:28:48 +0100

    diverse

--- app/src/main/java/easv/oe/tictactoe/MainActivity.kt
+++ app/src/main/java/easv/oe/tictactoe/MainActivity.kt
@@ -15,7 +15,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnCreate.setOnClickListener { v -> createBoard()}
+
+        btnCreate.setOnClickListener { _ -> createBoard()}
     }
 
     private fun createBoard() {
